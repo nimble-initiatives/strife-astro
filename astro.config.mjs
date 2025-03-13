f48d8ee8c7144fb74f611a4e818b962aa5d4bc06
Marcus Lindblom <m@marcuslindblom.com>
2025-03-13 14:31:29 +0100

    Use SSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 import strife from '@strifeapp/astro';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
+
+const { PROD } = import.meta.env;
 
 import Home from './src/collections/Home';
 // https://astro.build/config
@@ -15,7 +18,10 @@ export default defineConfig({
     port: 4322,
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), basicSsl()],
+    server: {
+      https: true,
+    },
     ssr: {
       noExternal: ["@strifeapp/strife"],
     },
