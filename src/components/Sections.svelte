1fe393a61bbbe4d62a58d3cfa4c5667490a33c3c
Marcus Lindblom <m@marcuslindblom.com>
2025-03-13 14:26:07 +0100

    Adds Bento

--- src/components/Sections.svelte
+++ src/components/Sections.svelte
@@ -4,6 +4,7 @@
   import Hero from './Hero.svelte';
   import RichText from './RichText.svelte';
   import Content from './Content.svelte';
+  import Bento from './Bento.svelte';
   import type { Chapter } from "@strifeapp/types";
   export let sections: Chapter[];
 
@@ -23,6 +24,9 @@
     {/if}
     {#if section['@strife'].template === 'text'}
       <RichText {...section} />
+    {/if}
+    {#if section['@strife'].template === 'bento'}
+      <Bento {...section} />
     {/if}
   {/each}
 </div>
